e3e79dd18052eead9db7c18e2ba4554b78784097
mooney <468035416@qq.com>
2018-02-01 13:49:15 +0800

    init

--- mrouter/src/main/java/mrouter/Mrouter.java
+++ mrouter/src/main/java/mrouter/Mrouter.java
@@ -3,11 +3,14 @@ package mrouter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -37,12 +40,22 @@ public enum Mrouter {
         routerCache=new HashMap<>();
     }
 
+    /**
+     * 打开指定路由的页面
+     * @param routerUri
+     * @return
+     */
     public boolean open(String routerUri){
 
         return open(routerUri, null);
     }
 
-
+    /**
+     * 打开指定路由的页面
+     * @param routerUri
+     * @param dataIntent
+     * @return
+     */
     public boolean open(String routerUri, Intent dataIntent){
 
         if (!MrouterHelper.isValidURI(routerUri)){
@@ -50,23 +63,45 @@ public enum Mrouter {
             return false;
         }
 
+        /**
+         * 打开web网页
+         */
         if (routerUri.startsWith("http://")||routerUri.startsWith("https://")){
 
             Uri uri = Uri.parse(routerUri); // url为你要链接的地址
 //            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            if (dataIntent==null){
+                dataIntent=new Intent();
+            }
             dataIntent.setAction(Intent.ACTION_VIEW);
             dataIntent.setData(uri);
             dataIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            try {
-                mContext.startActivity(dataIntent);
-                return true;
-            } catch (Exception e) {
-                Log.e(TAG, "Errors of opening web view with the router \""+routerUri+"\"!!!\n"+e);
-                return false;
-            }
 
+            return doOpen(null,routerUri,-1,dataIntent);
         }
 
+        /**
+         * 打开系统注册的路由activity
+         */
+        PackageManager packageManager = mContext.getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(dataIntent, 0);
+        if(activities.size() > 0){
+
+            if (dataIntent==null){
+                dataIntent=new Intent();
+            }
+
+            Uri uri = Uri.parse(routerUri);
+            dataIntent.setData(uri);
+            dataIntent.setAction(Intent.ACTION_VIEW);
+            dataIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            return doOpen(null,routerUri,-1,dataIntent);
+        }
+
+        /**
+         * 打开指定类名的activity
+         */
         Class activityClazz = findActivity(routerUri);
         if (activityClazz==null){
             Log.e(TAG, "there is no activity of the router \""+routerUri+"\"!!!");
@@ -84,17 +119,30 @@ public enum Mrouter {
                 dataIntent.putExtra(key, uri.getQueryParameter(key));
             }
         }
-
         dataIntent.setClass(mContext,activityClazz);
-        try {
-            mContext.startActivity(dataIntent);
-            return true;
-        } catch (Exception e) {
-            Log.e(TAG, "Errors of opening the router \""+routerUri+"\"!!!\n"+e);
-            return false;
-        }
+
+        return doOpen(null,routerUri,-1,dataIntent);
     }
 
+    /**
+     * 打开页面并监听回调
+     * @param context
+     * @param routerUri
+     * @param requestCode
+     * @return
+     */
+    public boolean openForResult(Object context, String routerUri, int requestCode){
+        return openForResult(context, routerUri, requestCode, null);
+    }
+
+    /**
+     * 打开页面并监听回调
+     * @param context
+     * @param routerUri
+     * @param requestCode
+     * @param dataIntent
+     * @return
+     */
     public boolean openForResult(Object context, String routerUri, int requestCode, Intent dataIntent) {
 
         if (MrouterHelper.isValidURI(routerUri)){
@@ -109,13 +157,6 @@ public enum Mrouter {
             dataIntent.setAction(Intent.ACTION_VIEW);
             dataIntent.setData(uri);
             dataIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            try {
-                mContext.startActivity(dataIntent);
-                return true;
-            } catch (Exception e) {
-                Log.e(TAG, "Errors of opening web view with the router \""+routerUri+"\"!!!\n"+e);
-                return false;
-            }
 
         }else {
 
@@ -142,28 +183,66 @@ public enum Mrouter {
 
 
 
-        if (context instanceof Activity){
+        return doOpen(context, routerUri, requestCode, dataIntent);
+
+    }
+
+
+    private boolean doOpen(Object context, String routerUri, int requestCode, Intent dataIntent){
+        if (context==null){
             try {
-                ((Activity)context).startActivityForResult(dataIntent,requestCode);
+                mContext.startActivity(dataIntent);
                 return true;
             } catch (Exception e) {
                 Log.e(TAG, "Errors of opening the router \""+routerUri+"\" for result!!!\n"+e);
+                return false;
+            }
+        }else if (context instanceof Activity){
+            try {
+                if (requestCode==-1){
+                    ((Activity)context).startActivity(dataIntent);
+                }else {
+                    ((Activity)context).startActivityForResult(dataIntent,requestCode);
+                }
+                return true;
+            } catch (Exception e) {
+                if (requestCode==-1){
+                    Log.e(TAG, "Errors of opening the router \""+routerUri+"\"!!!\n"+e);
+                }else {
+                    Log.e(TAG, "Errors of opening the router \""+routerUri+"\" for result!!!\n"+e);
+                }
                 return false;
             }
         }else if (context instanceof Fragment){
             try {
-                ((Fragment)context).startActivityForResult(dataIntent,requestCode);
+                if (requestCode==-1){
+                    ((Fragment)context).startActivity(dataIntent);
+                }else {
+                    ((Fragment)context).startActivityForResult(dataIntent,requestCode);
+                }
                 return true;
             } catch (Exception e) {
-                Log.e(TAG, "Errors of opening the router \""+routerUri+"\" for result!!!\n"+e);
+                if (requestCode==-1){
+                    Log.e(TAG, "Errors of opening the router \""+routerUri+"\"!!!\n"+e);
+                }else {
+                    Log.e(TAG, "Errors of opening the router \""+routerUri+"\" for result!!!\n"+e);
+                }
                 return false;
             }
         }else if (context instanceof android.app.Fragment){
             try {
-                ((Fragment)context).startActivityForResult(dataIntent,requestCode);
+                if (requestCode==-1){
+                    ((android.app.Fragment)context).startActivity(dataIntent);
+                }else {
+                    ((android.app.Fragment)context).startActivityForResult(dataIntent,requestCode);
+                }
                 return true;
             } catch (Exception e) {
-                Log.e(TAG, "Errors of opening the router \""+routerUri+"\" for result!!!\n"+e);
+                if (requestCode==-1){
+                    Log.e(TAG, "Errors of opening the router \""+routerUri+"\"!!!\n"+e);
+                }else {
+                    Log.e(TAG, "Errors of opening the router \""+routerUri+"\" for result!!!\n"+e);
+                }
                 return false;
             }
         }else {
@@ -171,7 +250,6 @@ public enum Mrouter {
                     "\nError:the context "+context.getClass().getCanonicalName()+" is not proper context!!!");
             return false;
         }
-
     }
 
 
